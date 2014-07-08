4938a9cf709f0bfdb3ea5a166ab6dcca1518c917
Asger Behncke Jacobsen <abj@asgers-mbp-2.fritz.box>
2014-07-08 19:38:14 +0200

    fix

--- lib/simple_form_localized_input/simple_form_localized_input.rb
+++ lib/simple_form_localized_input/simple_form_localized_input.rb
@@ -1,20 +1,22 @@
-class SimpleFormLocalizedInput < SimpleForm::FormBuilder
+module SimpleFormLocalizedInput
+  class Builder < SimpleForm::FormBuilder
 
-  # ---------------------------------------------------------------------
+    # ---------------------------------------------------------------------
 
-  def localized_input attribute_name, options={}
-    simple_fields_for "#{attribute_name}_translations", OpenStruct.new(object.send("#{attribute_name}_translations")) do |f|
-      I18n.available_locales.collect do |loc|
-        f.input loc.to_sym, options.merge(
-          {
-            collection: (options[:collection_translations] ? options[:collection_translations][loc.to_s] : options[:collection]),
-            label: [object.class.human_attribute_name(attribute_name), "(#{loc})"].reject(&:blank?).join(' ')
-          }
-        )
-      end.join.html_safe
+    def localized_input attribute_name, options={}
+      simple_fields_for "#{attribute_name}_translations", OpenStruct.new(object.send("#{attribute_name}_translations")) do |f|
+        I18n.available_locales.collect do |loc|
+          f.input loc.to_sym, options.merge(
+            {
+              collection: (options[:collection_translations] ? options[:collection_translations][loc.to_s] : options[:collection]),
+              label: [object.class.human_attribute_name(attribute_name), "(#{loc})"].reject(&:blank?).join(' ')
+            }
+          )
+        end.join.html_safe
+      end
     end
+
+    # ---------------------------------------------------------------------
+
   end
-
-  # ---------------------------------------------------------------------
-
 end
